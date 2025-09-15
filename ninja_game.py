0f5aaa9515dd284d699fa6d8a17b9900419e3f8a
tomux <thomasagomez77@gmail.com>
2025-09-14 23:32:29 -0500

    init with samuri charcter

---
+++ ninja_game.py
@@ -0,0 +1,337 @@
+import sys, os
+import random#partickles
+import math#particles
+import pygame
+from scripts.entities import PhysicsEntity, Player, Enemy
+from scripts.utils import load_image, load_images, Animation
+from scripts.tilemap import TileMap
+from scripts.clouds import Clouds
+from scripts.particle import Particle
+from scripts.spark import Spark
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('ninja game')
+        #game screen
+        self.screen = pygame.display.set_mode((800, 600))
+        #render size 'zoom' / 'camera' - both displays will merge seperating outlined from normal
+        self.display = pygame.Surface((320, 240), pygame.SRCALPHA)#objects withoutline
+        self.display_2 = pygame.Surface((320, 240))#objects without outline
+        #
+        self.clock = pygame.time.Clock()
+        #movement x y
+        self.movement = [False, False]
+
+        self.assets = {
+                #from scripts/utils.py
+                'decor' : load_images('tiles/decor'),
+                'grass' : load_images('tiles/grass'),
+                'large_decor' : load_images('tiles/large_decor'),
+                'stone' : load_images('tiles/stone'),
+                'player' : load_image('entities/player.png'),
+                'background' : load_image('background.png'),
+                'clouds': load_images('clouds'),
+                'enemy/idle': Animation(load_images('entities/enemy/idle'), img_dur=6),
+                'enemy/run': Animation(load_images('entities/enemy/run'), img_dur=4),
+                'player/idle': Animation(load_images('entities/samuri/idle'), img_dur=6),
+                'player/run': Animation(load_images('entities/samuri/run'), img_dur=4),
+                'player/jump': Animation(load_images('entities/samuri/jump'), img_dur=4),
+                'player/slide': Animation(load_images('entities/player/slide'), img_dur=4),
+                'player/wall_slide': Animation(load_images('entities/samuri/wall_slide'), img_dur=4),
+                'particle/leaf': Animation(load_images('particles/leaf'), img_dur=20, loop=False),
+                'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+                'gun': load_image('gun.png'),
+                'projectile': load_image('projectile.png'),
+                }
+
+        #sound effects
+        self.sfx = {
+                'jump': pygame.mixer.Sound('data/sfx/jump.wav'),
+                'dash': pygame.mixer.Sound('data/sfx/dash.wav'),
+                'hit': pygame.mixer.Sound('data/sfx/hit.wav'),
+                'shoot': pygame.mixer.Sound('data/sfx/shoot.wav'),
+                'ambience': pygame.mixer.Sound('data/sfx/ambience.wav'),
+                }
+
+        self.sfx['ambience'].set_volume(0.2)
+        self.sfx['shoot'].set_volume(0.4)
+        self.sfx['hit'].set_volume(0.8)
+        self.sfx['dash'].set_volume(0.3)
+        self.sfx['jump'].set_volume(0.7)
+
+        self.clouds = Clouds(self.assets['clouds'], count=16)
+        #from scripts/entities.py
+        self.player = Player(self, (50, 50), (8, 15))
+
+        #pass in assets to TileMap using self as the game
+        self.tilemap = TileMap(self, tile_size=16)
+
+        #start levels
+        self.level = 0
+        self.load_level()
+
+        #screen-shake effect
+        self.screenshake = 0
+
+    #level loader
+    def load_level(self):
+        #self.tilemap.load('data/maps/' + str(map_id) + '.json')
+        self.tilemap.load('map.json')
+        
+        #leaf spawners are trees ('large_decor', 2)
+        self.leaf_spawners = []
+        for tree in self.tilemap.extract([('large_decor', 2)], keep=True):
+            self.leaf_spawners.append(pygame.Rect(4 + tree['pos'][0], 4 + tree['pos'][1], 23, 13))
+
+        #physics entities spawners
+        self.enemies = []
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1)]):
+            if spawner['variant'] == 0:
+                self.player.pos = spawner['pos']
+                self.player.air_time = 0#prevents falling to death from multiple triggers
+            else:
+                self.enemies.append(Enemy(self, spawner['pos'], (8, 15)))
+        
+        #empty containers
+        self.projectiles = []
+        self.particles = []
+        self.sparks = []
+
+        #'camera' scroll
+        self.scroll = [0, 0]
+        #death
+        self.dead = 0
+        #transition
+        self.transition = -30
+
+    #game logic
+    def logic(self):
+            #--------------------logic------------------------
+        #level transition logic
+        if not len(self.enemies):#if no more enemies
+            self.transition += 1
+            if self.transition > 30:
+                self.level = min(self.level + 1, len(os.listdir('data/maps')) - 1)
+                #self.load_level(self.level)
+                self.load_level()
+        if self.transition < 0:
+            self.transition += 1
+        
+
+        #leaf particle logic
+        for rect in self.leaf_spawners:
+            #49999 makes the particals pass condition less often
+            if random.random() * 49999 < rect.width * rect.height:
+                pos_x = rect.x + random.random() * rect.width
+                pos_y = rect.y + random.random() * rect.height
+                pos = (pos_x, pos_y)
+                self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+        
+        #screenshake logic
+        self.screenshake = max(0, self.screenshake - 1) 
+
+        #render scroll logic
+        self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+        self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+        #convert to int to avoid jitters
+        render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+        return render_scroll
+    
+    def run(self):
+        #funcs
+        def manage_transition():
+            if self.transition:
+                transition_surface = pygame.Surface(self.display.get_size())
+                radius = (30 - abs(self.transition)) * 8
+                x_y = (self.display.get_width() // 2, self.display.get_height() // 2)
+                pygame.draw.circle(transition_surface, (255, 255, 255), x_y, radius)
+                transition_surface.set_colorkey((255, 255, 255))
+                self.display.blit(transition_surface, (0, 0))
+
+        def manage_particles():
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset=render_scroll)
+                if particle.p_type == 'leaf':
+                    #apply sin effect to position               0.035 slows down effect
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
+                if kill:
+                    self.particles.remove(particle)
+
+        #enemy logic
+        def manage_enemies():
+            for enemy in self.enemies.copy():
+                kill = enemy.update(self.tilemap, (0, 0))
+                enemy.render(self.display, offset=render_scroll)
+                if kill:
+                    self.enemies.remove(enemy)
+
+        #enemy projectile logic
+        def manage_enemy_projectiles():
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1]#[[x, y], direction, timer]
+                projectile[2] += 1
+                img = self.assets['projectile']
+                pos_x = projectile[0][0] - img.get_width() / 2 - render_scroll[0]
+                pos_y = projectile[0][1] - img.get_height() / 2 - render_scroll[1]
+                self.display.blit(img, (pos_x, pos_y))
+                if self.tilemap.solid_check(projectile[0]):#projectile hit wall
+                    self.projectiles.remove(projectile)
+                    #add parks
+                    for i in range(4):
+                        spark_pos = projectile[0]
+                        spark_angle = random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0)#bounce off walls
+                        spark_speed = 2 + random.random()
+                        new_spark = Spark(spark_pos, spark_angle, spark_speed)
+                        self.sparks.append(new_spark)
+                elif projectile[2] > 360:
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50:
+                    if self.player.rect().collidepoint(projectile[0]):#projectile hit player
+                        self.projectiles.remove(projectile)
+                        self.dead += 1#start death timer
+                        self.sfx['hit'].play(0)
+                        #screenshake
+                        self.screenshake = max(16, self.screenshake)
+                        for i in range(30):#burst effect of sparks and particles
+                            #add sparks
+                            spark_pos = self.player.rect().center
+                            spark_angle = random.random() * math.pi * 2
+                            spark_speed = random.random() * 5
+                            new_spark = Spark(spark_pos, spark_angle, spark_speed)
+                            self.sparks.append(new_spark)
+                            #add particles
+                            pos = self.player.rect().center
+                            velocity_x = math.cos(spark_angle + math.pi) * spark_speed * 0.5
+                            velocity_y = math.sin(spark_angle + math.pi) * spark_speed * 0.5
+                            particle_velocity = [velocity_x, velocity_y]
+                            new_particle = Particle(self, 'particle', pos, velocity=particle_velocity, frame=random.randint(0, 7))
+                            self.particles.append(new_particle)
+
+        #death logic
+        def manage_death():
+            if self.dead:
+                self.dead += 1
+                if self.dead >= 10:#ensure positive transition is rendered
+                    self.transition = min(30, self.transition + 1)#without triggering next level
+                if self.dead > 40:
+                    self.load_level()
+
+        #sparks from projectiles logic
+        def manage_projectile_sparks():
+            for spark in self.sparks.copy():
+                kill = spark.update()
+                spark.render(self.display, offset=render_scroll)
+                if kill:
+                    self.sparks.remove(spark)
+
+        def manage_screenshake():
+                mod = (random.random() * self.screenshake) - (self.screenshake / 2)
+                return (mod, mod)
+
+        def manage_mask():#wont effect renders called after
+            display_mask = pygame.mask.from_surface(self.display)#mask for respective renders
+            display_sillouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0))#make less transparent
+            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:#cast sillouette on all sides
+                self.display_2.blit(display_sillouette, offset)
+        
+        #bg music
+        pygame.mixer.music.load('data/music.wav')
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)#0, 1, -1
+        #bg sfx
+        self.sfx['ambience'].play(-1)
+
+
+            #-------------------gameloop------------------------
+
+        while True:
+            render_scroll = self.logic()
+            
+            self.display.fill((0, 0, 0, 0))#fill forground with black transparant for mask
+            #backgrond img
+            self.display_2.blit(self.assets['background'], (0, 0))
+
+            #death check
+            manage_death()
+
+            #clouds behind tilemap
+            self.clouds.update()
+            self.clouds.render(self.display_2, offset=render_scroll)
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            #enemy calls
+            manage_enemies()
+
+            #player calls
+            if not self.dead:
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+                self.player.render(self.display, offset=render_scroll)
+
+            #enemy projectile calls
+            manage_enemy_projectiles()
+
+            #spark from projectiles calls
+            manage_projectile_sparks()
+
+            #manage sillouette before particles
+            #manage_mask()
+            
+            #leaf particles calls
+            manage_particles()
+                
+            #input
+            self.keyboard_input()
+                
+            #manage transition
+            manage_transition()
+
+            #display
+            self.display_2.blit(self.display, (0, 0))#draw game back on top / "merge displays"
+            screenshake_offset = manage_screenshake()
+            self.screen.blit(pygame.transform.scale(self.display_2, self.screen.get_size()), screenshake_offset)#render displays
+            pygame.display.update()
+            self.clock.tick(60)
+
+    def keyboard_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.movement[0] = True
+                if event.key == pygame.K_RIGHT:
+                    self.movement[1] = True
+                if event.key == pygame.K_UP:
+                    if self.player.jump():
+                        self.sfx['jump'].play(0)
+                if event.key == pygame.K_x:
+                    if self.player.dash():
+                        self.sfx['dash'].play(0)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    self.movement[0] = False
+                if event.key == pygame.K_RIGHT:
+                    self.movement[1] = False
+
+Game().run()
+
+#
+#
+#
+
+def controller_input():
+    #d-pad
+    if event.type == pygame.JOYHATMOTION:
+        hat_x, hat_y = event.value                    
+        self.movement[0] = hat_x == -1  # Left → True if pressed
+        self.movement[1] = hat_x == 1   # Right → True if pressed
+    #buttons
+    if event.type == pygame.JOYBUTTONDOWN:
+        if event.button == 0:# A button
+            self.player.jump()
+        if event.button == 2:# X button
+            self.player.dash()
---
+++ scripts/tilemap.py
@@ -0,0 +1,160 @@
+import json
+import pygame
+
+AUTOTILE_MAP = {
+        #if tile right and below use tile 0
+        tuple(sorted([(1, 0), (0, 1)])): 0,#sorted for consistancy and tuple for key-value
+        #right, below, and to left
+        tuple(sorted([(1, 0), (0, 1), (-1, 0)])): 1,
+        #left, below
+        tuple(sorted([(-1, 0), (0, 1)])): 2,
+        #left, above, below
+        tuple(sorted([(-1, 0), (0, -1), (0, 1)])): 3,
+        #left, above
+        tuple(sorted([(-1, 0), (0, -1)])): 4,
+        #left, above, right
+        tuple(sorted([(-1, 0), (0, -1), (1, 0)])): 5,
+        #right, above
+        tuple(sorted([(1, 0), (0, -1)])): 6,
+        #right, above, below
+        tuple(sorted([(1, 0), (0, -1), (0, 1)])): 7,
+        #all
+        tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 8
+        }
+#variable for tile locations around player
+NEIGHBORS_OFFSETS = [(-1, 0), (-1,-1), (0,-1), (1,-1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {'grass', 'stone'}
+AUTOTILE_TYPES = {'grass', 'stone'} 
+
+class TileMap:
+    def __init__(self, game,tile_size=16):
+        self.game = game
+        self.tile_size = tile_size
+        self.tilemap = {}
+        self.offgrid_tiles = []
+
+
+        # the tiles will be saved as strings of dictionary values
+
+    #extracts tiles offgrid only
+    def extract(self, id_pairs, keep=False):
+        matches = []
+        for tile in self.offgrid_tiles.copy():#
+            if (tile['type'], tile['variant']) in id_pairs:
+                matches.append(tile.copy())
+                if not keep:
+                    self.offgrid_tiles.remove(tile)
+
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            if (tile['type'], tile['variant']) in id_pairs:
+                matches.append(tile.copy())
+                matches[-1]['pos'] = matches[-1]['pos'].copy()
+                matches[-1]['pos'][0] *= self.tile_size
+                matches[-1]['pos'][1] *= self.tile_size
+                if not keep:
+                    del self.tilemap[loc]
+
+        return matches
+
+    def tiles_around(self, pos):
+        tiles = []
+        #
+        tile_loc_x = int(pos[0] // self.tile_size)
+        tile_loc_y = int(pos[1] // self.tile_size) 
+        tile_loc = (tile_loc_x, tile_loc_y)#tuple..
+        for offset in NEIGHBORS_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+
+    #makes tile editor eaiser to work with
+    def autotile(self):
+        for loc in self.tilemap:
+            tile = self.tilemap[loc]
+            neighbors = set()
+            #exclude diagnals from offset neighbors
+            for shift in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+                check_loc = str(tile['pos'][0] + shift[0]) + ';' + str(tile['pos'][1] + shift[1])
+                if check_loc in self.tilemap:
+                    if self.tilemap[check_loc]['type'] == tile['type']:
+                        neighbors.add(shift)
+            neighbors = tuple(sorted(neighbors))
+            if (tile['type'] in AUTOTILE_TYPES) and (neighbors in AUTOTILE_MAP):
+                tile['variant'] = AUTOTILE_MAP[neighbors]
+                    
+
+    def load(self, path):
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+        
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+
+    
+    #save to json
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close()
+
+    def solid_check(self, pos):
+        #convert pixel pos to coridnates of the grid system
+        tile_loc = str(int(pos[0] // self.tile_size)) + ';' + str(int(pos[1] // self.tile_size))
+        if tile_loc in self.tilemap:#check if tile exits
+            if self.tilemap[tile_loc]['type'] in PHYSICS_TILES:#check if physics tile
+                return self.tilemap[tile_loc]
+
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                tile_pos_x_by_size = tile['pos'][0] * self.tile_size 
+                tile_pos_y_by_size = tile['pos'][1] * self.tile_size 
+                rects.append(pygame.Rect(tile_pos_x_by_size, tile_pos_y_by_size, self.tile_size, self.tile_size))
+        return rects
+
+
+    def render(self, surface, offset=(0, 0)):
+        #
+        #tilemap blits
+        for tile in self.offgrid_tiles:
+            tile_type = tile['type']
+            tile_variant = tile['variant']
+            tile_pos = tile['pos']
+            tile_pos_offset = (tile_pos[0] - offset[0], tile_pos[1] - offset[1]) 
+            img = self.game.assets[tile_type][tile_variant] 
+            surface.blit(img, tile_pos_offset)
+
+        #x range lookup
+        start_x = offset[0] // self.tile_size
+        end_x = (offset[0] + surface.get_width()) // self.tile_size + 1
+        #y range lookup
+        start_y = offset[1] // self.tile_size
+        end_y = (offset[1] + surface.get_height()) // self.tile_size + 1
+        for x in range(start_x, end_x):
+            for y in range(start_y, end_y):
+                loc = str(x) + ';' + str(y)
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    tile_type = tile['type']
+                    tile_variant = tile['variant']
+                    tile_pos = tile['pos']
+                    tile_pos_x_os = tile_pos[0] * self.tile_size - offset[0] 
+                    tile_pos_y_os = tile_pos[1] * self.tile_size - offset[1] 
+                    tile_pos_offset = (tile_pos_x_os, tile_pos_y_os) 
+                    img = self.game.assets[tile_type][tile_variant] 
+                    surface.blit(img, tile_pos_offset)
+            
+
+        #for loc in self.tilemap:
+            #tile = self.tilemap[loc]
+            #tile_type = tile['type']
+            #tile_variant = tile['variant']
+            #tile_pos = tile['pos']
+            #surface.blit(self.game.assets[tile_type][tile_variant], (tile_pos[0] * self.tile_size - offset[0], tile_pos[1] * self.tile_size - offset[1]))
+
+
